20e445709e7bfa9fb702c2b90bfd59ce5d35b95e
= <=>
2024-07-01 21:01:09 +0100

    adding new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
 	root "articles#index"
 	
-	get "/article", to: "articles#index"
+	resources :articles
 	
-	get "/articles/:id", to: "articles#show"
+	# get "/article", to: "articles#index"
+	
+	# get "/articles/new", to: "articles#new"
+	
+	# get "/articles/:id", to: "articles#show"
+	
+	# post "/articles", to: "articles#create"
+	
+	# get "/articles/:id/edit", to: "articles#edit"
+	
+	# patch "/articles/:id", to: "article#update"
+	
+	# delete "/articles/:id", to: "article#destroy"
 	
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
